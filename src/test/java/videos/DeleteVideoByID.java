193aef0ac27f0994c0e88c780cf7188823e2d98c
Lydia Gammoh <gammohlydia94@gmail.com>
2026-02-08 23:00:37 +0300

    use the delete method in rest assured

---
+++ src/test/java/videos/DeleteVideoByID.java
@@ -0,0 +1,12 @@
+package videos;
+
+import static io.restassured.RestAssured.given;
+
+public class DeleteVideoByID {
+    public static void main(String[] args) {
+        given().baseUri("http://localhost:8080")
+                .pathParams("id",1)
+                .when().delete("/videos/{id}")
+                .then().log().all();
+    }
+}
