51b124c2520dc0e8197b25a1a8ed54fff86949d0
Lydia Gammoh <gammohlydia94@gmail.com>
2026-02-12 13:40:23 +0300

    extract response using rest assured

--- src/test/java/courses/GetCourseByID.java
+++ src/test/java/courses/GetCourseByID.java
@@ -1,13 +1,21 @@
 package courses;
 
+import io.restassured.response.Response;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class GetCourseByID {
     public static void main(String[] args) {
-        given().baseUri("http://localhost:8080")
-                .pathParam("id", 1)
-                .when().get("/course/{id}")
-                .then().log().all()
-                .assertThat().statusCode(200);
+       Response response= given().baseUri("http://localhost:8080")
+                .pathParam("id", "7c5b17f8-8231-4fe1-b556-32c1f3b82b7d")
+                .when().get("/courses/{id}")
+                .then().extract().response();
+
+       int status = response.statusCode();
+       String id = response.path("id");
+       float price = response.path("price");
+       int numOfVideos = response.path("numOfVideos");
+        System.out.println(status+id+price+numOfVideos);
     }
 }
--- src/test/java/courses/GetCourses.java
+++ src/test/java/courses/GetCourses.java
@@ -1,13 +1,20 @@
 package courses;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
 
 public class GetCourses {
     public static void main(String[] args) {
-
+        Header header = new Header("API_KEY","QAcart");
         RestAssured.given().baseUri("http://localhost:8080")
+                .header(header)
                 .when().get("/courses")
                 .then().log().all()
-                .assertThat().statusCode(200);
+                .assertThat().statusCode(200)
+                .assertThat().body("title", hasItem("Selenium WebDriver"));
+//                .assertThat().body("[1].title", equalTo("Selenium WebDriver"));
     }
 }
